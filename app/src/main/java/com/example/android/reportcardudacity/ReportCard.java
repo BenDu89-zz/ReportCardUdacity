9998cfa48c43ea366b0466d351aeea3572c1f8a9
BenDu89 <Xy2er889>
2017-06-10 14:58:23 +0200

    seconde review Feedback

--- app/src/main/java/com/example/android/reportcardudacity/ReportCard.java
+++ app/src/main/java/com/example/android/reportcardudacity/ReportCard.java
@@ -5,79 +5,89 @@ package com.example.android.reportcardudacity;
  */
 
 public class ReportCard {
-    /* Math grade of the student*/
-    private int mathGrade;
-    /* Biologic grade of the student*/
-    private int bioGrade;
-    /* German grade of the student*/
-    private int germanGrade;
-    /* Informatics grade of the student*/
-    private int infoGrade;
-    /* Sports grade of the student*/
-    private int sportGrade;
-    /* Name of the student*/
-    private String studentName;
     /* Name of the University*/
     public static final String university = "Udacity";
+    /* Math grade of the student*/
+    private int mmathGrade;
+    /* Biologic grade of the student*/
+    private int mbioGrade;
+    /* German grade of the student*/
+    private int mgermanGrade;
+    /* Informatics grade of the student*/
+    private int minfoGrade;
+    /* Sports grade of the student*/
+    private int msportGrade;
+    /* Name of the student*/
+    private String mstudentName;
 
     /* Set the values for the Reportard*/
-    public ReportCard(int MatheGrade, int BiologicGrade, int GermanGrade, int InformaticsGrade, int SportGrade, String Studentname) {
-        mathGrade = MatheGrade;
-        bioGrade = BiologicGrade;
-        germanGrade = GermanGrade;
-        infoGrade = InformaticsGrade;
-        sportGrade = SportGrade;
-        studentName = Studentname;
+    public ReportCard(int matheGrade, int biologicGrade, int germanGrade, int informaticsGrade, int sportGrade, String studentname) {
+        mmathGrade = matheGrade;
+        mbioGrade = biologicGrade;
+        mgermanGrade = germanGrade;
+        minfoGrade = informaticsGrade;
+        msportGrade = sportGrade;
+        mstudentName = studentname;
 
     }
     /* Get the Math Grade*/
-    public int getmathGrade() {
-        return mathGrade;
+    public int getMathGrade() {
+        return mmathGrade;
     }
-    /* Get the Bio Grade*/
-    public int getbioGrade() {
-        return bioGrade;
-    }
-    /* Get the German Grade*/
-    public int getgermanGrade() {
-        return germanGrade;
-    }
-    /* Get the Infomatics Grade*/
-    public int getinfoGrade() {
-        return infoGrade;
-    }
-    /* Get the Sports Grade*/
-    public int getsportGrade() {
-        return sportGrade;
-    }
-    /* Set the Bio Grade*/
-    public void setBioGrade(int bioGrade) {
-        this.bioGrade = bioGrade;
-    }
-    /* Set the German Grade*/
-    public void setGermanGrade(int germanGrade) {
-        this.germanGrade = germanGrade;
-    }
-    /* Set the Informatics Grade*/
-    public void setInfoGrade(int infoGrade) {
-        this.infoGrade = infoGrade;
-    }
+
     /* Set the Math Grade*/
     public void setMathGrade(int mathGrade) {
-        this.mathGrade = mathGrade;
+        this.mmathGrade = mathGrade;
     }
+
+    /* Get the Bio Grade*/
+    public int getBioGrade() {
+        return mbioGrade;
+    }
+
+    /* Set the Bio Grade*/
+    public void setBioGrade(int bioGrade) {
+        this.mbioGrade = bioGrade;
+    }
+
+    /* Get the German Grade*/
+    public int getGermanGrade() {
+        return mgermanGrade;
+    }
+
+    /* Set the German Grade*/
+    public void setGermanGrade(int germanGrade) {
+        this.mgermanGrade = germanGrade;
+    }
+
+    /* Get the Infomatics Grade*/
+    public int getInfoGrade() {
+        return minfoGrade;
+    }
+
+    /* Set the Informatics Grade*/
+    public void setInfoGrade(int infoGrade) {
+        this.minfoGrade = infoGrade;
+    }
+
+    /* Get the Sports Grade*/
+    public int getSportGrade() {
+        return msportGrade;
+    }
+
     /* Set the Sports Grade*/
     public void setSportGrade(int sportGrade) {
-        this.sportGrade = sportGrade;
+        this.msportGrade = sportGrade;
     }
+
     /* Return in a readable format*/
     @Override
     public String toString () {
-        return "Name of the student: " + studentName + '\n' +
-                "Math grade: " + mathGrade + '\n' +
-                "Bioligie grade: " + bioGrade + '\n' +
-                "German grade: " + germanGrade + '\n' +
-                "Informatics grade: " + infoGrade + '\n' +
-                "Sport grade: " + sportGrade + '\n';
+        return "Name of the student: " + mstudentName + '\n' +
+                "Math grade: " + mmathGrade + '\n' +
+                "Bioligie grade: " + mbioGrade + '\n' +
+                "German grade: " + mgermanGrade + '\n' +
+                "Informatics grade: " + minfoGrade + '\n' +
+                "Sport grade: " + msportGrade + '\n';
     }
 }
