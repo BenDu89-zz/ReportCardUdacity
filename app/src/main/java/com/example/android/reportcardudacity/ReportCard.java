bec8a1afef89a6b26a2fc8dd36238c6c4f05f7c9
BenDu89 <Xy2er889>
2017-06-10 12:31:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/reportcardudacity/ReportCard.java
@@ -0,0 +1,78 @@
+package com.example.android.reportcardudacity;
+
+/**
+ * Created by benjamindunisch on 10.06.17.
+ */
+
+public class ReportCard {
+    /* Math grade of the student*/
+    private int mathGrade;
+    /* Biologic grade of the student*/
+    private int bioGrade;
+    /* German grade of the student*/
+    private int germanGrade;
+    /* Informatics grade of the student*/
+    private int infoGrade;
+    /* Sports grade of the student*/
+    private int sportGrade;
+    /* Name of the student*/
+    public String StudentName = "Benjamin";
+
+    /* Set the values for the Reportard*/
+    public ReportCard(int MatheGrade, int BiologicGrade, int GermanGrade, int InformaticsGrade, int SportGrade) {
+        mathGrade = MatheGrade;
+        bioGrade = BiologicGrade;
+        germanGrade = GermanGrade;
+        infoGrade = InformaticsGrade;
+        sportGrade = SportGrade;
+    }
+    /* Get the Math Grade*/
+    public int getmathGrade() {
+        return mathGrade;
+    }
+    /* Get the Bio Grade*/
+    public int getbioGrade() {
+        return bioGrade;
+    }
+    /* Get the German Grade*/
+    public int getgermanGrade() {
+        return germanGrade;
+    }
+    /* Get the Infomatics Grade*/
+    public int getinfoGrade() {
+        return infoGrade;
+    }
+    /* Get the Sports Grade*/
+    public int getsportGrade() {
+        return sportGrade;
+    }
+    /* Set the Bio Grade*/
+    public void setBioGrade(int bioGrade) {
+        this.bioGrade = bioGrade;
+    }
+    /* Set the German Grade*/
+    public void setGermanGrade(int germanGrade) {
+        this.germanGrade = germanGrade;
+    }
+    /* Set the Informatics Grade*/
+    public void setInfoGrade(int infoGrade) {
+        this.infoGrade = infoGrade;
+    }
+    /* Set the Math Grade*/
+    public void setMathGrade(int mathGrade) {
+        this.mathGrade = mathGrade;
+    }
+    /* Set the Sports Grade*/
+    public void setSportGrade(int sportGrade) {
+        this.sportGrade = sportGrade;
+    }
+    /* Return in a readable format*/
+    public String toString () {
+        return "Name of the student: " + StudentName + '\n' +
+                "Math grade: " + mathGrade + '\n' +
+                "Bioligie grade: " + bioGrade + '\n' +
+                "German grade: " + germanGrade + '\n' +
+                "Informatics grade: " + infoGrade + '\n' +
+                "Sport grade: " + sportGrade + '\n';
+    }
+}
